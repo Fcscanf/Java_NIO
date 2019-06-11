b939ad209a1dccb2fe6bd65bbcdee5df3af9cf07
Fcscanf <fcscanf@gmail.com>
2019-06-11 11:23:14 +0800

    [Codes]:Mybatis-非自增式主键值的获取

---
+++ Spring/src/main/java/com/fcant/mybatis/ch01/bean/Log.java
@@ -0,0 +1,18 @@
+package com.fcant.mybatis.ch01.bean;
+
+import lombok.Data;
+
+/**
+ * Log
+ * <p>
+ * encoding:UTF-8
+ *
+ * @author Fcscanf
+ * @description
+ * @date 上午 10:25 2019-06-11/0011
+ */
+@Data
+public class Log {
+    private int logid;
+    private String msg;
+}
